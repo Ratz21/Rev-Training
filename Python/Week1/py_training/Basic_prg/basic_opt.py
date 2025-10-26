12cfbcc33e58913249f42948242450d6a09b4200
StudentRajdeep <rajdeepparbhane11@gmail.com>
2025-10-26 09:42:22 +0530

    New learning added

---
+++ Python/Week1/py_training/Basic_prg/basic_opt.py
@@ -0,0 +1,12 @@
+from platform import version
+# running basic programs
+print("hello world")
+
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter another number: "))
+print('sum: ',num1 + num2)
+print('difference: ',num1 - num2)
+print('Product: ',num1 * num2)
+print('int Div: ',num1 // num2)
+print('remainder', num1 % num2)
+print('Power: ', num1 ** num2)
---
+++ Python/Week1/py_training/Basic_prg/big2.py
@@ -0,0 +1,46 @@
+'''
+Multiline commands using commas
+Date: 25-10-2025
+Author: Rajdeep
+Biggest of 2 nos
+'''
+
+
+# num1 = int(input("Enter a number: "))
+# num2 = int(input("Enter another number: "))
+#
+# if num1 > num2:
+#     print(f"{num1} > {num2}") # fstring in py
+# else:
+#     print(f"{num1} > {num2}")
+
+
+
+
+    price = 85
+    money = f"{price}dollars" # why it said to me remove redundant parenthesis
+    print(money)
+
+    # functions
+    # relations first  finally logic
+
+    # # predefined / system / built in  / lambda function
+    # def fun(parameters):
+    #     print(parameters)
+
+      #fname (arguments) -> arguments are the actual data #paramaters #recieve the data
+
+# def add():
+#     num1 = int(input("Enter a number: "))
+#     num2 = int(input("Enter another number: "))
+#     value = num1 + num2
+#     return value
+
+def add_input():
+    n1 = int(input("Enter first number: "))
+    n2 = int(input("Enter second number: "))
+    result = n1 + n2
+    print(n1+n2)
+add_input()
+# without calling  the function it cannot execute the function
+# RISHA
